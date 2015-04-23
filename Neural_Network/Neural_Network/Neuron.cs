06294f5c197b6c59e37105175e71497204d85df5
Mirko Speth <mirko.speth@gmail.com>
2015-04-23 15:59:54 +0200

    vs project

---
+++ Neural_Network/Neural_Network/Neuron.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Neural_Network
+{
+    class Neuron : Cell
+    {
+        private List<Synapse> incomingSynpases = new List<Synapse>();
+        private List<Synapse> outgoingSynpases = new List<Synapse>();
+        private List<double> weights = new List<double>();
+
+        public Neuron()
+        {
+
+        }
+    
+        public void addIncomingSynapse(Synapse s)
+        {
+            incomingSynpases.Add(s);
+        }
+    }
+}
